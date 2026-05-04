13cedea9a3f07672734f457aacaabc5506b64d0e
toddn <tcnichol@illinois.edu>
2026-05-04 16:22:11 -0500

    download with dates options works

--- utils/download_dynamic_world_data.py
+++ utils/download_dynamic_world_data.py
@@ -4,7 +4,9 @@ from loguru import logger
 from water_timeseries.downloader import EarthEngineDownloader
 import nest_asyncio
 import sys
+from pathlib import Path
 import asyncio
+import datetime
 # Set the event loop policy for better compatibility
 if sys.platform == 'darwin':  # macOS
     asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
@@ -18,6 +20,13 @@ try:
 except RuntimeError:
     pass
 
+def human_file_size(path):
+    size = path.stat().st_size
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if size < 1024.0:
+            return f"{size:.1f} {unit}"
+        size /= 1024.0
+    return f"{size:.1f} PB"
 
 def load_config(config_path="/app/config/config.toml"):
     """Load configuration from TOML file"""
@@ -42,16 +51,20 @@ def main():
         config_path = args.config
     config = load_config(config_path=config_path)
     EE_PROJECT = config["EE_PROJECT"]
+    os.environ["EE_PROJECT"] = EE_PROJECT
     VECTOR_DATASET = config["VECTOR_DATASET"]
     DOWNLOAD_DIR = config["DOWNLOAD_DIR"]
     START_YEAR = config["START_YEAR"]
     END_YEAR = config["END_YEAR"]
     START_MONTH = config["START_MONTH"]
     END_MONTH = config["END_MONTH"]
-    FORMAT = 'nc'
+    FORMAT = config["FORMAT"]
     APPEND = config["APPEND"]
     ENTIRE = config["ENTIRE"]
     # if not append, download file for the given range
+    CURRENT_TIMESTAMP = str(datetime.datetime.now()).replace(" ", "_")
+    NEW_FILENAME = 'dynamic_world_data_' +CURRENT_TIMESTAMP + '.' + FORMAT
+    DOWNLOAD_FILEPATH = os.path.join(DOWNLOAD_DIR, NEW_FILENAME)
     if ENTIRE:
         print(f"Download all data from start to present day in a new file")
     elif APPEND:
@@ -60,7 +73,20 @@ def main():
         print(f"Download all data from start to the end")
         MONTHS = list(range(START_MONTH, END_MONTH + 1))
         YEARS = list(range(START_YEAR, END_YEAR + 1))
+        dl = EarthEngineDownloader(ee_auth=True, logger=logger)
+        ds = dl.download_dw_monthly(
+            vector_dataset=VECTOR_DATASET,
+            name_attribute="id_geohash",
+            years=YEARS,
+            months=MONTHS,
+            save_to_file=str(DOWNLOAD_FILEPATH),
+        )
         print('months and years')
+        new_file_exists = os.path.exists(DOWNLOAD_FILEPATH)
+        if new_file_exists:
+            print(f"New file {DOWNLOAD_FILEPATH} exists")
+            readable_filesize = human_file_size(Path(DOWNLOAD_FILEPATH))
+            print(f"New file {DOWNLOAD_FILEPATH} size: {readable_filesize}")
 
 
     print("Done")
