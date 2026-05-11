7e4b18128ade5d707737e7c7b477d6767ec7e600
toddn <tcnichol@illinois.edu>
2026-05-11 14:33:41 -0500

    fix right class

--- download/download_dynamic_world_by_month_year.py
+++ download/download_dynamic_world_by_month_year.py
@@ -30,6 +30,8 @@ if ',' in str(year):  # Convert to string to handle if year is already a list
         new_year = int(part.strip())
         new_years.append(new_year)
     year = new_years
+else:
+    year = [int(year)]
 
 vector_dataset_path = os.path.join(base_dir, 'input', 'Nitze_etal_Lakes_filtered_full_set_V2d.parquet')
 
