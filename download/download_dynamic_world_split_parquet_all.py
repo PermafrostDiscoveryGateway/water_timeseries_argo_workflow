7477df60e937df36903a0d71748b83d939da8bb8
toddn <tcnichol@illinois.edu>
2026-05-12 12:47:23 -0500

    8 workers now

--- download/download_dynamic_world_split_parquet_all.py
+++ download/download_dynamic_world_split_parquet_all.py
@@ -71,7 +71,7 @@ for split_vector_file in all_split_vector_files:
             months=all_months,
             save_to_file=output_filepath,
             max_total_requests=2000,
-            n_parallel=1,
+            n_parallel=8,
         )
         logger.debug(f"Finished downloading all years for {split_vector_file}")
 
