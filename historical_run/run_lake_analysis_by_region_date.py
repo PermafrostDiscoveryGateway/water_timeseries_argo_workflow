5f5e77bd3b76b4a2601a365fbba307a4b0279007
toddn <tcnichol@illinois.edu>
2026-06-10 09:25:13 -0500

    using earlier date/time

--- historical_run/run_lake_analysis_by_region_date.py
+++ historical_run/run_lake_analysis_by_region_date.py
@@ -390,7 +390,7 @@ def main():
 
     # Call the analysis function with sample parameters
     REGION = "TEST"
-    ANALYSIS_DATE = "2026-05"
+    ANALYSIS_DATE = "2024-06"
 
     run_water_timeseries_analysis(REGION, ANALYSIS_DATE)
 
