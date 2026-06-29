866ab95a6e790f60e4cfeeff1e8c8729a05f959f
toddn <tcnichol@illinois.edu>
2026-06-29 17:26:57 -0500

    main method for the test run run this an argo to see results

--- near_real_time/run_test_near_real_time_v2.py
+++ near_real_time/run_test_near_real_time_v2.py
@@ -1,5 +1,6 @@
 from near_real_time.near_real_time_grid_v2 import download_near_real_time_region, download_new_dynamic_world_data, \
-    verify_downloads_complete, merge_near_real_time_region, compare_netcdf_files, process_near_real_time_region_dates, generate_expected_dates
+    verify_downloads_complete, merge_near_real_time_region, compare_netcdf_files, process_near_real_time_region_dates, \
+    generate_expected_dates, download_near_real_time_region_dates
 from near_real_time_grid import near_real_time_region
 import sys
 from loguru import logger
@@ -37,15 +38,17 @@ def main():
 
     expected_dates = generate_expected_dates(start_year=2025)
 
-    # download_near_real_time_region(region="TEST", run_start_label=start_label)
+    # download_near_real_time_region_dates(region="TEST", run_start_label=start_label, dates_to_download=expected_dates)
     verify_downloads_complete(region="TEST", run_start_label=start_label, analysis_dates=analysis_dates)
     result = merge_near_real_time_region(region="TEST", run_start_label=start_label, dates_to_merge=analysis_dates)
+    new_file_path = result['result']
+    new_file_path_string = str(new_file_path)
     logger.debug(f"Done for test")
 
     # new_netcdf_file_path = '/Users/helium/Desktop/dynamic_world/data/historical_data_20260629_104025.nc'
     # original_netcdf_file_path = '/Users/helium/Desktop/dynamic_world/data/lakes_dw_V2d.nc'
-    # logger.debug(f"Comparing the netcdf files {new_netcdf_file_path} to original {original_netcdf_file_path}")
-    # # compare_netcdf_files(original_netcdf_file_path, new_netcdf_file_path)
+    logger.debug(f"Comparing the netcdf files {new_file_path_string} to original {original_most_recent_dynamic_world_file}")
+    compare_netcdf_files(original_most_recent_dynamic_world_file, new_file_path_string)
     logger.debug(f"Comparison finished")
 
     process_near_real_time_region_dates(region="TEST", current_analysis_dates=original_missing_dates)
