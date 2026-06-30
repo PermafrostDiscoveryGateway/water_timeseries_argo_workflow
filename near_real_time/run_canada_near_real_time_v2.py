3efd41dfeeedb64a5b53e269140216140fcff482
toddn <tcnichol@illinois.edu>
2026-06-30 13:07:05 -0500

    make near real time canada identity to TEST

--- near_real_time/run_canada_near_real_time_v2.py
+++ near_real_time/run_canada_near_real_time_v2.py
@@ -1,6 +1,6 @@
 from near_real_time.near_real_time_grid_v2 import download_near_real_time_region, download_new_dynamic_world_data, \
     verify_downloads_complete, merge_near_real_time_region, compare_netcdf_files, process_near_real_time_region_dates, \
-    generate_expected_dates, download_near_real_time_region_dates
+    generate_expected_dates, download_near_real_time_region_dates, process_near_real_time_region_dates_zarr
 from near_real_time_grid import near_real_time_region
 import sys
 from loguru import logger
@@ -23,14 +23,14 @@ def main():
 
     dynamic_world_data_dir = os.environ['dynamic_world_data']
     all_dynamic_world_files = glob.glob(os.path.join(dynamic_world_data_dir, "*.nc"))
-    original_most_recent_dynamic_world_file = min(all_dynamic_world_files, key=os.path.getctime)
+    original_most_recent_dynamic_world_file = max(all_dynamic_world_files, key=os.path.getctime)
+    logger.debug(f"Original most recent dynamic world file: {original_most_recent_dynamic_world_file}")
 
     now = datetime.now()
     start_label = now.strftime("%Y%m%d%H%M%S")
 
     analysis_dates = []
     original_missing_dates = utils.download_new_dynamic_world_data.check_missing_data_in_netcdf(original_most_recent_dynamic_world_file)
-    latest_date = max(original_missing_dates)
     for date_idx, date in enumerate(original_missing_dates):
         date_type = type(date)
         print(date_type)
@@ -39,23 +39,21 @@ def main():
 
     expected_dates = generate_expected_dates(start_year=2025)
 
-
     download_near_real_time_region_dates(region="CANADA", run_start_label=start_label, dates_to_download=expected_dates)
     verify_downloads_complete(region="CANADA", run_start_label=start_label, analysis_dates=analysis_dates)
-    merge_near_real_time_region(region="CANADA", run_start_label=start_label, dates_to_merge=analysis_dates)
-    logger.debug(f"Done for test")
+    result = merge_near_real_time_region(region="CANADA", run_start_label=start_label, dates_to_merge=analysis_dates)
+    logger.debug(result)
+    if result is not None and 'result' in result:
+        new_file_path = result['result']
+        new_file_path_string = str(new_file_path)
+        logger.debug(f"Comparing the netcdf files {new_file_path_string} to original {original_most_recent_dynamic_world_file}")
+        compare_netcdf_files(original_most_recent_dynamic_world_file, new_file_path_string)
+        logger.debug(f"Comparison finished")
+    else:
+        logger.debug(f"No result no merge required")
 
-    all_dynamic_world_files = glob.glob(os.path.join(dynamic_world_data_dir, "*.nc"))
-    new_most_recent_dynamic_world_file = min(all_dynamic_world_files, key=os.path.getctime)
-
-    # new_netcdf_file_path = '/Users/helium/Desktop/dynamic_world/data/historical_data_20260629_104025.nc'
-    # original_netcdf_file_path = '/Users/helium/Desktop/dynamic_world/data/lakes_dw_V2d.nc'
-    logger.debug(f"Comparing the netcdf files {new_most_recent_dynamic_world_file} to original {original_most_recent_dynamic_world_file}")
-    compare_netcdf_files(original_most_recent_dynamic_world_file, new_most_recent_dynamic_world_file)
-    logger.debug(f"Comparison finished")
-
-    process_near_real_time_region_dates(region="CANADA", current_analysis_dates=original_missing_dates)
-
+    process_near_real_time_region_dates_zarr(region="CANADA", current_analysis_dates=expected_dates)
+    logger.debug(f"Finished processing")
 
 if __name__ == "__main__":
     main()
