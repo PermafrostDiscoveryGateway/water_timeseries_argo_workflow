0ae01d3f669ec7da3fe537fb638a02c76e324007
toddn <tcnichol@illinois.edu>
2026-07-07 13:27:08 -0500

    fix the most recent file in the download script

--- near_real_time/download_region.py
+++ near_real_time/download_region.py
@@ -101,7 +101,7 @@ def main():
     summer_months = [6, 7, 8, 9]
     dynamic_world_data_dir = os.environ['dynamic_world_data']
     all_dynamic_world_files = glob.glob(os.path.join(dynamic_world_data_dir, "*.nc"))
-    original_most_recent_dynamic_world_file = max(all_dynamic_world_files, key=os.path.getctime)
+    original_most_recent_dynamic_world_file = max(all_dynamic_world_files, key=lambda f: Path(f).stat().st_mtime)
     logger.debug(f"This is the most recent dynamic world file {original_most_recent_dynamic_world_file}")
     missing_dates_from_netcdf = utils.download_new_dynamic_world_data.check_missing_data_in_netcdf(original_most_recent_dynamic_world_file)
 
